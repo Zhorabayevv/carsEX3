8643ead502fb592c01aaf7d1f749018a6a3d9c30
Zhorabaevv <nzhorabaev777@gmail.com>
2023-01-16 04:01:32 +0600

    with validator

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function validateForm(){
+    let name = document.getElementById("name");
+    let phone = document.getElementById("phone");
+    let carText = document.getElementById("car-text");
+    if(name.value.length < 3 || name.value.length > 30){
+        let error = document.getElementById("errorName");
+        error.classList.remove("correct");
+        error.classList.add("error");
+    } else if(phone.value.length < 10 || phone.value.length > 15){
+        let error = document.getElementById("errorPhone");
+        error.classList.remove("correct");
+        error.classList.add("error");
+    } else if(carText.value.length < 3 || carText.value.length > 30){
+        let error = document.getElementById("errorCar");
+        error.classList.remove("correct");
+        error.classList.add("error");
+    }
+}
+
+document.getElementById("main-btn").addEventListener("click", function () {
+    document.getElementById("cars").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+});
+
+let btns = document.getElementsByClassName("car-btn");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        document.getElementById("price").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    });
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     let layer = document.querySelector('.price-img');
 document.addEventListener('mousemove', (event) => {
